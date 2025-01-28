ef2b98ce9bbec476900a5db0c5d07c75099c1035
Katarzyna Krawczyk <kasiakrawczyk999@gmail.com>
2025-01-28 17:50:35 +0100

    Add methods for calculating sum and Pi using Leibniz formula
    - Implement CalculateNumbersToReachSum method to determine the number of integers needed to reach a specified sum.
    
    - Implement CalculatePiUsingLeibnizFormula method to calculate the value of Pi using the Leibniz formula.
    
    - Update variable names to English in CalculatePiUsingLeibnizFormula.
    
    - Add menu options to call the new methods from the main program.

--- Program.cs
+++ Program.cs
@@ -4,7 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
+        initializeMenu();
+    }
+
+    private static void ShowMenu()
+    {
+        Console.WriteLine("1.ReachSumOfNumbers ");
+        Console.WriteLine("2. CalculateNumbersToReachSum");
+        Console.WriteLine("3.CalculatePiUsingLeibnizFormula");
+        Console.WriteLine("4. Exit");
+    }
+
+    private static void initializeMenu()
+    {
         IterativeStatement interactiveStatement = new IterativeStatement();
-        interactiveStatement.ReachSumOfNumbers();
+        bool exit = false;
+        while (!exit)
+        {
+            ShowMenu();
+            string option = Console.ReadLine();
+            switch (option)
+            {
+                case "1":
+                    Console.WriteLine("ReachSumOfNumbers starting...");
+                    interactiveStatement.ReachSumOfNumbers();
+                    break;
+                case "2":
+                    Console.WriteLine("CalculateNumbersToReachSum starting...");
+                    interactiveStatement.CalculateNumbersToReachSum();
+                    break;
+                case "3":
+                    Console.WriteLine("CalculatePiUsingLeibnizFormula starting...");
+                    interactiveStatement.CalculatePiUsingLeibnizFormula();
+                    break;
+                case "4":
+                    Console.WriteLine("Exiting");
+                    exit = true;
+                    break;
+                default:
+                    Console.WriteLine("Invalid option");
+                    break;
+            }
+        }
     }
 }
